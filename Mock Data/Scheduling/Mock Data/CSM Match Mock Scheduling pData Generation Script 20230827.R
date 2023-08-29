cf2aaa858828dde8fefc7f60f4a97556214faf02
Daniel Conroy-Beam <you@example.com>
2023-08-29 12:10:33 -0700

    Revising scheduling for within-subjects and ignoring match with RAs

--- Mock Data/Scheduling/Mock Data/CSM Match Mock Scheduling pData Generation Script 20230827.R
+++ Mock Data/Scheduling/Mock Data/CSM Match Mock Scheduling pData Generation Script 20230827.R
@@ -1,5 +1,5 @@
 #Create a dataframe with participant information
-pData<-data.frame("PIN"=c(sample(1:50),sample(51:100)),"lPIN"=c(c(sample(1:50),sample(51:100))),
+pData<-data.frame("PIN"=c(sample(1:50),sample(51:100)),
                   "sex"=rep(c(0,1),each=50))
 
 #Create a dummy match variable
@@ -9,10 +9,6 @@ pData$match[51:100]<-pData$PIN[match(pData$PIN[51:100],pData$match[1:50])]
 #Create a dummy email variable
 pData$email<-sapply(1:nrow(pData),function(x) 
   paste0(sample(letters,5),collapse=""))
-
-pData$link<-sapply(1:nrow(pData),function(x)
-  paste0("qualtrics.com/","?",pData$lPIN[x],collapse="")
-  )
 
 write.csv(pData,
           "Mock Data/Scheduling/Mock Data/Mock Scheduling pData 20230828.csv",
